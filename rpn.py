d7db224cf6a97e4128795488d7a8249d8d0155e8
Wesley Pederson <pedersonw2015@gmail.com>
2018-10-26 11:49:54 -0400

    did something

--- rpn.py
+++ rpn.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
+import operator
+
+op = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.floordiv}
 
 def calculate(arg):
-    line = arg.split(' ')
-    left = []
-    left.append(line.pop_front())
-    left.append(line.pop_front())
-    while():
-        if(line.top() 
-        
-    
-
+    stack = arg.split()
+    while len(stack) > 1:
+        token = stack.pop()
+        try:
+            val = int(token)
+            stack.append(val)
+        except ValueError:
+            val2 = int(stack.pop())
+            val1 = int(stack.pop())
+            func = op[token]
+            result = func(val1, val2)
+            stack.append(str(result))
+    return int(stack[0])
 
 def main():
     while True:
--- test_unit.py
+++ test_unit.py
@@ -1,7 +1,23 @@
 import unittest
 import rpn
 
-class TestBasics(unittest.Testcase):
+class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
+    
+    def test_sub(self):
+        result = rpn.calculate('1 1 -')
+        self.assertEqual(0, result)
+    
+    def test_mul(self):
+        result = rpn.calculate('2 2 *')
+        self.assertEqual(4, result)
+
+    def test_div(self):
+        result = rpn.calculate('6 3 /')
+        self.assertEqual(2, result)
+
+    def test_chain(self):
+        result = rpn.calculate('1 1 + 2 *')
+        self.assertEqual(4, result)
