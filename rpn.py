44d8cc8e61a21eb1b21c2095bbb76a0f27e5300f
Wesley Pederson <pedersonw2015@gmail.com>
2018-11-07 23:45:39 -0500

    add finished shit

--- rpn.py
+++ rpn.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
+
 import operator
 
-op = {
+
+operators = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
-    '/': operator.floordiv}
+    '/': operator.truediv,
+}
 
-def calculate(arg):
-    stack = arg.split()
-    while len(stack) > 1:
-        token = stack.pop()
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
         try:
-            val = int(token)
-            stack.append(val)
+            token = int(token)
+            stack.append(token)
         except ValueError:
-            val2 = int(stack.pop())
-            val1 = int(stack.pop())
-            func = op[token]
-            result = func(val1, val2)
-            stack.append(str(result))
-    return int(stack[0])
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
-        calculate(input('rpn calc> '))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
